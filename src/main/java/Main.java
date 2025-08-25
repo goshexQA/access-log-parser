d1224ebd0e3499148b83d74c0987a4173bb6b3e0
SamokhvalovIgor <goshex121@gmail.com>
2025-08-26 00:48:18 +0300

    add new homework number-experiments

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,18 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        try (Scanner scanner = new Scanner(System.in)) {
-            System.out.println("Введите текст и нажмите <Enter>: ");
-            String inputText = scanner.nextLine();
-            System.out.println("Длина введённого текста: " + inputText.length() + " символов.");
-
-        }
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите первое число:");
+        int firstNumber = scanner.nextInt();
+        System.out.println("Введите второе число:");
+        int secondNumber = scanner.nextInt();
+        int sum = firstNumber + secondNumber;
+        System.out.println("Сумма: "+sum);
+        int vich = firstNumber - secondNumber;
+        System.out.println("Разность: "+vich);
+        int product = firstNumber * secondNumber;
+        System.out.println("Произведение: "+product);
+        double quotient = (double) firstNumber / secondNumber;
+        System.out.println("Частное: " + quotient);
     }
-}
+    }
